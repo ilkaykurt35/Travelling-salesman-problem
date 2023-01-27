aff88d877a59488d5d414c4880f7bd0ea8041486
ilkaykurt35 <lkykrt@gmail.com>
2023-01-27 21:29:10 +0300

    Point Class created

--- gezgin satıcı problemi.py
+++ gezgin satıcı problemi.py
@@ -3,3 +3,31 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random
 import string
+
+class Point():
+    listOfInstances = []
+    def __init__(self, name, x, y):
+        self.name = name
+        self.x = x
+        self.y = y
+        self.visited = False
+        Point.listOfInstances.append(self)
+    def calculateWay(self):
+        pass
+    def nearestPoint(self):
+        distances = []
+        instances = []
+        for i in Point.listOfInstances:
+            if i.visited == False and i is not self:
+                distances.append(Point.distanceBetweenPoints(self, i))
+                instances.append(i)
+        if distances.count(min(distances)) > 1:
+            # return self.calculateWay()
+            return instances[distances.index(min(distances))]
+        else:
+            return instances[distances.index(min(distances))]
+        
+    @staticmethod
+    def distanceBetweenPoints(a,b):
+        ab = math.sqrt((abs(a.x-b.x))**2+(abs(a.y-b.y))**2)
+        return ab 
